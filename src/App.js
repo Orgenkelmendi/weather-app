0ddafff4e729b40d1f0b055a521252a369bd4642
Orgen Kelmendi <okelmendi@gmail.com>
2024-05-16 11:51:38 -0400

    App completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [city,setCity] = useState('');
+  const [data,setData] = useState(null)
+
+  const url = `http://localhost:5000/weather?city=${city}`
+  const today = new Date()
+
+  const searchCity = async (event) => {
+    if (event.key === 'Enter') {
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+        console.log(response.data);
+        setCity('')
+      } catch (error) {
+        console.error('Error fetching weather data:',error);
+      }
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='header'>
+        <h1>Weather App</h1>
+      </div>
+      <div className='input'>
+        <input
+          type='text'
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
+          onKeyDown={searchCity}
+          placeholder='Enter city name'
+        />
+      </div>
+      <div className='date'>
+        {data ? <h4>{`${data.name}, ${data.sys.country}`}</h4> : null}
+        {data ? <p>{`${today.getMonth()} / ${today.getDate()} / ${today.getFullYear()}`}</p> : null}
+      </div>
+      <div className='weather'>
+        {data ? <h2>{(data.main.temp).toFixed()} °F <span> {data.weather[0].main}</span></h2> : null}
+        {data ? <div className='weatherExtra'>
+          <div>
+            <p>Feels like:</p>
+            {data ? <h2>{(data.main.feels_like).toFixed()} °F</h2> : null}
+          </div>
+          <div>
+            <p>Humidity:</p>
+            {data ? <h2>{(data.main.humidity).toFixed()} %</h2> : null}
+          </div>
+          <div>
+            <p>Wind speed:</p>
+            {data ? <h2>{(data.wind.speed).toFixed()} mp/h</h2> : null}
+          </div>
+        </div> : null }
+      </div>
+
     </div>
   );
 }
